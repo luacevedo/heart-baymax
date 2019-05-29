2393f30b48aeec81e0ee470b7edd854693f24cd6
Luciana Acevedo <luacevedo90@gmail.com>
2019-05-29 11:19:34 -0300

    changed comments

--- app/src/main/java/com/example/luacevedo/heartbaymax/MainActivity.java
+++ app/src/main/java/com/example/luacevedo/heartbaymax/MainActivity.java
@@ -28,9 +28,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //TODO: change this
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
